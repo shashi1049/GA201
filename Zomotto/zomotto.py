dd1f9e4e5124c2d3b9f6404b3335b3cb266a39ad
shashi1049 <shashi290396@gmail.com>
2023-07-06 02:04:23 +0530

    zesty zomotto

--- Zomotto/zomotto.py
+++ Zomotto/zomotto.py
@@ -1,185 +1,147 @@
-from prettytable import PrettyTable
-from collections import OrderedDict
-from colorama import Fore, Style
-
-
-menu = OrderedDict({})
+menu = {}
 orders = {}
-order_id_counter = 1
-
-# for adding dishesh
 
 
-def add():
-    while True:
-        try:
-            id = int(input("Enter dish ID: "))
-            name = input("Enter dish name: ")
-            price = float(input("Enter price: "))
-            stock = int(input("Enter quantity: "))
-            availablility = "Not Available"
-            if int(stock) >= 1:
-                availablility = "Available"
-
-            menu[id] = {
-                "Name": name,
-                "Price": price,
-                "Stock": stock,
-                "Availability": availablility
-            }
-
-            print(f"Dish {name} with ID {id} added to menu.")
-            viewMenu()
-            break
-        except ValueError:
-            print("Invalid input. Try again!")
-
-# to remove a dish from menu
-
-
-def remove():
-    id = int(input("Enter the dish ID to remove: "))
-    if id in menu:
-        name = menu[id]["Name"]
-        del menu[id]
-        print(f"Dish '{name}' with ID '{id}' removed from the menu.")
-        viewMenu()
-    else:
-        print("Dish not found in the menu.")
-
-
-# viewing the menu
-def viewMenu():
-    # Create the table
-    table = PrettyTable()
-    table.field_names = ['Dish ID', 'Dish Name',
-                         'Price', 'Stock', 'Availability']
-
-    # Add data to the table
-    for dish_id, dish_details in menu.items():
-        dish_name = dish_details["Name"]
-        dish_price = dish_details["Price"]
-        dish_stock = dish_details["Stock"]
-        dish_availability = dish_details["Availability"]
-
-        # Set color based on availability
-        if dish_availability == "Available":
-            dish_availability = f"{Fore.GREEN}{dish_availability}{Style.RESET_ALL}"
-        else:
-            dish_availability = f"{Fore.RED}{dish_availability}{Style.RESET_ALL}"
-
-        table.add_row([dish_id, dish_name, dish_price,
-                      dish_stock, dish_availability])
-
-    # Print the table
-    print(table)
-
-
-# taking order from customer
-def takeOrder():
-    global order_id_counter
-    customer_name = input("Enter customer name: ")
-    dish_id = int(input("Enter dish ID: "))
-    quan = int(input("Enter quantity: "))
-    dishes_ordered = []
-    if dish_id in menu and menu[dish_id]["Availability"] == "Available":
-        dishes_ordered.append(menu[int(dish_id)]["Name"])
-        if menu[dish_id]["Stock"] < quan:
-            print("Can't order more than", menu[dish_id]["Stock"])
-            return
-        else:
-            menu[dish_id]["Stock"] = menu[dish_id]["Stock"] - quan
-    else:
-        print(
-            f"Dish with ID '{dish_id}' is not available. Order cannot be placed.")
+def add_dish():
+    dish_id = input("Enter dish ID: ")
+    if dish_id in menu:
+        print("Dish with ID", dish_id, "already exists!")
         return
 
-    order = {
-        "Customer Name": customer_name,
-        "Dishes Ordered": dishes_ordered,
-        "Status": "Received"
-    }
-    orders[order_id_counter] = order
-    print(f"Order ID: {order_id_counter}")
-    order_id_counter += 1
-    reviewOrders()
-
-# update order status
+    dish_name = input("Enter dish name: ")
+    dish_price = float(input("Enter dish price: "))
+    dish_stock = int(input("Enter dish stock: "))
+    dish_availability = "Yes" if dish_stock > 0 else "No"
+    menu[dish_id] = {"id": dish_id, "name": dish_name, "price": dish_price,
+                     "stock": dish_stock, "availability": dish_availability}
+    print("Dish added successfully!")
 
 
-def updateOrderStatus():
-    order_id = int(input("Enter order ID: "))
-    if order_id in orders:
-        order = orders[order_id]
-        print(f"Current Status: {order['Status']}")
-        print("1. Preparing")
-        print("2. Ready for Pickup")
-        print("3. Delivered")
-        option = int(input("Select an option to update the status: "))
+def update_dish():
+    dish_id = input("Enter dish ID to update: ")
+    if dish_id not in menu:
+        print("Dish with ID", dish_id, "not found!")
+        return
 
-        if option == 1:
-            order['Status'] = "Preparing"
-        elif option == 2:
-            order['Status'] = "Ready for Pickup"
-        elif option == 3:
-            order['Status'] = "Delivered"
-        else:
-            print("Invalid option selected.")
+    dish = menu[dish_id]
+    dish['name'] = input("Enter new dish name: ")
+    dish['price'] = float(input("Enter new dish price: "))
+    dish['stock'] = int(input("Enter new dish stock: "))
+    dish['availability'] = "Yes" if dish['stock'] > 0 else "No"
+    print("Dish updated successfully!")
+
+
+def remove_dish():
+    dish_id = input("Enter dish ID to remove: ")
+    if dish_id not in menu:
+        print("Dish with ID", dish_id, "not found!")
+        return
+
+    del menu[dish_id]
+    print("Dish removed successfully!")
+
+
+def take_order():
+    customer_name = input("Enter customer name: ")
+    dish_id = input("Enter dish ID to order: ")
+    if dish_id not in menu:
+        print("Dish with ID", dish_id, "not found!")
+        return
+
+    dish = menu[dish_id]
+    if dish['availability'] == "Yes" and dish['stock'] > 0:
+        dish['stock'] -= 1  # Decrease stock count by 1
+        if dish['stock'] == 0:
+            dish['availability'] = "No"
+        dish['availability'] = "Ready"
+        order_id = len(orders) + 1
+        orders[order_id] = {"Customer Name": customer_name,
+                            "Dish ID": dish_id, "Status": "Ready"}
+        print("Order placed successfully! Order ID:", order_id)
     else:
-        print("Order not found.")
+        print("Sorry, the dish is not available or out of stock.")
 
 
-# review orders
-def reviewOrders():
-    # Create the table
-    table = PrettyTable()
-    table.field_names = ['Order ID',
-                         'Customer Name', 'Dishes Ordered', 'Status']
+def update_order_status():
+    order_id = int(input("Enter order ID to update status: "))
+    if order_id not in orders:
+        print("Order with ID", order_id, "not found!")
+        return
 
-    # Add data to the table
+    order = orders[order_id]
+    new_status = input(
+        "Enter new status (Received, Pending, Ready, Delivered): ")
+    order['Status'] = new_status
+    print("Order status updated successfully!")
+
+
+def review_orders():
+    print("All Orders:")
+    print("{:<10} {:<20} {:<10} {:<10}".format(
+        "Order ID", "Customer Name", "Dish ID", "Status"))
     for order_id, order_details in orders.items():
-        customer_name = order_details["Customer Name"]
-        dishes_ordered = ', '.join(order_details["Dishes Ordered"])
-        status = order_details["Status"]
-
-        table.add_row([order_id, customer_name, dishes_ordered, status])
-
-    # Print the table
-    print(table)
+        print("{:<10} {:<20} {:<10} {:<10}".format(
+            order_id, order_details['Customer Name'], order_details['Dish ID'], order_details['Status']))
+    print()
 
 
-def runApp():
+def view_menu():
+    print("Menu:")
+    print("{:<10} {:<20} {:<10} {:<10}".format(
+        "Dish ID", "Dish Name", "Dish Price", "Availabilty"))
+    for dish_id, dish in menu.items():
+        print("{:<10} {:<20} {:<10} {:<10}".format(
+            dish['id'], dish['name'], dish['price'], dish['availability']))
+    print()
+
+
+def exit_app():
+    print("Thank you for using Zomato Chronicles: The Great Food Fiasco!")
+    return
+
+
+def show_menu():
+    print("Zomato Chronicles: The Great Food Fiasco")
+    print("1. Add a new dish")
+    print("2. Update a dish")
+    print("3. Remove a dish")
+    print("4. Take an order")
+    print("5. Update the status of an order")
+    print("6. Review all orders")
+    print("7. View Menu")
+    print("8. Exit")
+
+
+def handle_input(choice):
+    if choice == '1':
+        add_dish()
+    elif choice == '2':
+        update_dish()
+    elif choice == '3':
+        remove_dish()
+    elif choice == '4':
+        take_order()
+    elif choice == '5':
+        update_order_status()
+    elif choice == '6':
+        review_orders()
+    elif choice == '7':
+        view_menu()
+    elif choice == '8':
+        exit_app()
+    else:
+        print("Invalid choice!")
+
+
+def main():
     while True:
-        print("1. Add dish to menu")
-        print("2. Remove dish from menu")
-        print("3. Update availability of particular dish")
-        print("4. Take order")
-        print("5. Update order status")
-        print("6. Review all orders")
-        print("7. View Menu")
-        print("8. Exit")
-
-        choice = input("Enter selection: ")
-        if choice == "1":
-            add()
-        elif choice == "2":
-            remove()
-        elif choice == "3":
-            print(choice)
-        elif choice == "4":
-            takeOrder()
-        elif choice == "5":
-            updateOrderStatus()
-        elif choice == "6":
-            reviewOrders()
-        elif choice == "7":
-            viewMenu()
-        elif choice == "8":
-            break
-        else:
-            print("Invalid selection, try again!")
+        show_menu()
+        choice = input("Enter your choice (1-8): ")
+        handle_input(choice)
+        print()
 
 
-runApp()
+if __name__ == '__main__':
+    main()
 
-print("Thanks for using 'Zomato Chronicles: The Great Food Fiasco App'")
+print(orders)
