f2b5db8f1bf8d06cccc773c3d96e75cfeef1a394
shashi1049 <shashi290396@gmail.com>
2023-07-05 10:24:09 +0530

    String1

---
+++ String/String1.py
@@ -0,0 +1,19 @@
+
+# concate String
+s = "Amay"
+print(s)        # Amay
+s = s+'a'
+print(s)        # Amaya
+
+
+# indexing and slicing of String
+
+s = "Amay"
+print(s[-3:-1])        # ma
+print(s[:2])           # Am
+
+
+# declaration of String
+
+s = "Masai School"
+print(s)
