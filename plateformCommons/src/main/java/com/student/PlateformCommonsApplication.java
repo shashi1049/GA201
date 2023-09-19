fbaa7dc1930991cb322d6d5709e29ea90cc8446c
shashi1049 <shashi290396@gmail.com>
2023-09-19 23:23:40 +0530

    platformCommons

---
+++ plateformCommons/src/main/java/com/student/PlateformCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformCommonsApplication.class, args);
+	}
+
+}
