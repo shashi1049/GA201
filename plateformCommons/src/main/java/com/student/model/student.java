1b76e0dd910c0a2db1f4596305aea283d2598d14
shashi1049 <shashi290396@gmail.com>
2023-09-19 23:46:41 +0530

    platformCommons

---
+++ plateformCommons/src/main/java/com/student/model/student.java
@@ -0,0 +1,42 @@
+package com.student.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer studentId;
+	private String name;
+	private LocalDate dob;
+	private Gender gender;
+	
+	@Embedded
+	@ElementCollection
+	private List<StudentAddress> studentAddress;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "students")
+	private List<Course> courses;
+
+}
