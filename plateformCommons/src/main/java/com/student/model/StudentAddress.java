161ef507fb873abae498c90b0be974f6c1900e77
shashi1049 <shashi290396@gmail.com>
2023-09-19 23:51:59 +0530

    platformCommons

--- plateformCommons/src/main/java/com/student/model/StudentAddress.java
+++ plateformCommons/src/main/java/com/student/model/StudentAddress.java
@@ -1,5 +1,18 @@
 package com.student.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentAddress {
+	
+	private String area;
+	private String district;
+	private String state;
+	private String pincode;
+	private String addressType;
 
 }
